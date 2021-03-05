8268efdd0fbaa2dce7fd55c51a2a8f45df86ce43
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-03-05 16:11:37 +0200

    Fixed test1

--- src/qccdParser.jl
+++ src/qccdParser.jl
@@ -7,11 +7,11 @@ include("types.jl")
 # Creates a topology using a graph from JSON
 function createTopology(path::String)::SimpleDiGraph{Int64}
     if !isfile(path)
-        throw(ArgumentError(path + " Is not a file"))
+        throw(ArgumentError("Input is not a file"))
     end
     # Parsing JSON
     topology = JSON3.read(read(path, String), Topology)
-
+    
     # Initialize graphs
     nodesAdjacency = topology.adjacency.nodes
     topology = DiGraph(length(nodesAdjacency))
