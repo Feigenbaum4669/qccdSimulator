860ebb4c9cfb3131ba2b85458a376e1cc5b7c9f1
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-03-12 17:36:12 +0200

    Initialization of junctions

--- src/qccdParser.jl
+++ src/qccdParser.jl
@@ -1,27 +1,26 @@
 using LightGraphs
 import JSON3
 include("./types/json.jl")
+include("types/device.jl")
 
 #=  
     Input -> Json path
     Output -> Topology
-    Creates a topology using a graph from JSON 
-=#
+    Creates a topology using a graph from JSON =#
 function createTopology(path::String)::SimpleDiGraph{Int64}
     topology::TopologyJSON  = try 
         _readJSON(path::String)
     catch err
         throw(err)
     end
-    
+    junctions = _createJunctions(topology.shuttle.shuttles, topology.junction.junctions)
     return _createGraph(topology)
 end
 
 #=  
     Input -> Json path
     Output -> TopologyJSON
-    Creates an object topologyJSON from JSON 
-=#
+    Creates an object topologyJSON from JSON =#
 function _readJSON(path::String)::TopologyJSON
     if !isfile(path)
         throw(ArgumentError("Input is not a file"))
@@ -37,8 +36,7 @@ end
 #=  
     Input -> topologyJSON
     Output -> graph
-    Creates a graph using a object topologyJSON
-=#
+    Creates a graph using a object topologyJSON =#
 function _createGraph(topology::TopologyJSON)::SimpleDiGraph{Int64}
     # Initialize graphs
     nodesAdjacency::Dict{String,Array{Int64}} = topology.adjacency.nodes
@@ -53,3 +51,25 @@ function _createGraph(topology::TopologyJSON)::SimpleDiGraph{Int64}
     return graphTopology
 end
 
+#=  
+ =#
+function _createJunctions(shuttles::Array{ShuttleInfoJSON}, junctions::Array{JunctionInfoJSON})::Dict{Int64,Junction}
+    res = Dict{Int64,Junction}()
+    for j ∈ junctions
+        if haskey(res, j.id)
+            throw(ArgumentError(string("Repeated junction ID:", j.id, ". Make sure the IDs are unique.")))
+        end
+        connectedShuttles = Iterators.filter(x -> x.from == j.id || x.to == j.id, shuttles)
+        junctionEnds = Dict(s.id => JunctionEnd() for s ∈ connectedShuttles)
+        try
+            res[j.id] = Junction(j.id, eval(Meta.parse(j.type)), junctionEnds)
+        catch e
+            if e isa UndefVarError
+                throw(ArgumentError(string("Junction type ", j.type, " not supported")))
+            else
+                rethrow(e)
+            end
+        end
+    end
+    return res
+end
--- src/types/device.jl
+++ src/types/device.jl
@@ -1,7 +1,17 @@
 include("json.jl")
 
-@enum JunctionEndStatus free blocked 
-@enum JunctionType t x y
+@enum JunctionEndStatus begin 
+    free
+    blocked
+end
+@enum JunctionType begin 
+    T 
+    Y
+    X 
+end
+
+# Supported junction types with corresponding sizes
+const typesSizes = Dict(T => 3, Y => 3, X => 4)
 
 #=  Struct for junction end.
     queue: Array of qubits waiting in the junction end (if any) 
@@ -16,12 +26,17 @@ end
 #=  Struct for junction.
     id: Junction ID.
     type: Type of the junction. Each type may define how the junction works differently
-    ends: Dict with key being the shuttle ID the junction is connected to and value a JunctionEnd =#
+    ends: Dict with key being the shuttle ID the junction is connected to and value a JunctionEnd
+    @Throws ArgumentError if junction type doesn't match with number of ends. =#
 struct Junction
     id::Int64 
     type::JunctionType
-    ends::Dict{Int64,JunctionEnd}
-    function Junction(junctionJSON::JunctionJSON)
-        
+    ends::Dict{String,JunctionEnd}
+    function Junction(id::Int64, type::JunctionType, ends::Dict{String,JunctionEnd})
+        if length(ends) != typesSizes[type]
+            throw(ArgumentError(string("Junction with ID ", id, " of type ",type, " has ",length(ends),
+            " ends. It should have ", typesSizes[type], " ends.")))
+        end
+        return new(id, type, ends)
     end
 end
