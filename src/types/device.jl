8e571377a089634d962847587bec54abdb138a41
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-03-12 13:30:35 +0200

    Junction related types and enums in device.jl

---
+++ src/types/device.jl
@@ -0,0 +1,25 @@
+include("json.jl")
+
+@enum JunctionEndStatus free blocked 
+@enum JunctionType t x y
+
+#=  Struct for junction end.
+    queue: Array of qubits waiting in the junction end (if any) 
+    status: Status of the junction end, either free (queue is empty) or blocked otherwise =#
+struct JunctionEnd
+    queue::Array{String,1}
+    status::JunctionEndStatus
+end
+
+#=  Struct for junction.
+    id: Junction ID.
+    type: Type of the junction. Each type may define how the junction works differently
+    ends: Dict with key being the shuttle ID the junction is connected to and value a JunctionEnd =#
+struct Junction
+    id::Int64 
+    type::JunctionType
+    ends::Dict{Int64,JunctionEnd}
+    function Junction(junctionJSON::JunctionJSON)
+        
+    end
+end
