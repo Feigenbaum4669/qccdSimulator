fe6f9bf0d201caef878dd9393f535ae53e649da0
Hopery <anabel.ovide@gmail.com>
2021-03-12 13:33:12 +0200

    Split createTopology function

--- src/qccdParser.jl
+++ src/qccdParser.jl
@@ -2,10 +2,27 @@ using LightGraphs
 import JSON3
 include("./types/typesJson.jl")
 
-# Input -> Json path
-# Output -> Topology
-# Creates a topology using a graph from JSON
+#=  
+    Input -> Json path
+    Output -> Topology
+    Creates a topology using a graph from JSON 
+=#
 function createTopology(path::String)::SimpleDiGraph{Int64}
+    topology::TopologyJSON  = try 
+        _readJSON(path::String)
+    catch err
+        throw(err)
+    end
+    
+    return _createGraph(topology)
+end
+
+#=  
+    Input -> Json path
+    Output -> TopologyJSON
+    Creates an object topologyJSON from JSON 
+=#
+function _readJSON(path::String)::TopologyJSON
     if !isfile(path)
         throw(ArgumentError("Input is not a file"))
     end
@@ -15,6 +32,14 @@ function createTopology(path::String)::SimpleDiGraph{Int64}
     catch err
         throw(ArgumentError(err.msg))
     end
+end
+
+#=  
+    Input -> topologyJSON
+    Output -> graph
+    Creates a graph using a object topologyJSON
+=#
+function _createGraph(topology::TopologyJSON)::SimpleDiGraph{Int64}
     # Initialize graphs
     nodesAdjacency::Dict{String,Array{Int64}} = topology.adjacency.nodes
     graphTopology::SimpleDiGraph{Int64} = DiGraph(length(nodesAdjacency))
@@ -27,3 +52,4 @@ function createTopology(path::String)::SimpleDiGraph{Int64}
     end
     return graphTopology
 end
+
