cb13f2aff5fd2b26bb70e71ac127e19c94a07205
Hopery <anabel.ovide@gmail.com>
2021-03-25 22:17:54 +0200

    Change function names and add functionality

--- src/qccdSimulator.jl
+++ src/qccdSimulator.jl
@@ -1,7 +1,21 @@
 module qccdSimulator
+include("./types/QCCDevDescription.jl")
 
-test()::Bool = true
+"""
+Creates an object topologyJSON from JSON.
+Throws ArgumentError an error if input is not a valid file.
+"""
+function readJSON(path::String)::QCCDevDescription
+    if !isfile(path)
+        throw(ArgumentError("Input is not a file"))
+    end
+    # Parsing JSON
+    return topology::QCCDevDescription  = try 
+        JSON3.read(read(path, String), TopologyJSON)
+    catch err
+        throw(ArgumentError(err.msg))
+    end
+end
 
-
-export test
+export readJSON
 end
