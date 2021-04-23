ec21f9d21f3e97050be850c87096b8a1da761167
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-04-23 18:08:48 +0300

    Qubit ids from Symbol to Int

--- src/qccdevcontrol.jl
+++ src/qccdevcontrol.jl
@@ -46,7 +46,7 @@ struct QCCDevCtrl
 
     t_now          ::Time_t
 # Descomment when load() function is done
-#    qubits      ::Dict{String,Qubit}
+#    qubits      ::Dict{Int,Qubit}
     traps          ::Dict{Symbol,Trap}
     junctions      ::Dict{Symbol,Junction}
     shuttles       ::Dict{Symbol,Shuttle}
