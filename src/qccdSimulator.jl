e905bb0ee7792a65a94e727fcb7a7fc89085ea1a
alejandrovgonzalez <alexvilloria26@gmail.com>
2021-04-30 21:48:23 +0300

    Reading operation times implemented and tested

--- src/qccdSimulator.jl
+++ src/qccdSimulator.jl
@@ -1,5 +1,5 @@
 module qccdSimulator
-export readJSON
+export readJSON, readTimeJSON
 
 include("./types/description.jl")
 include("./types/control.jl")
@@ -22,6 +22,28 @@ function readJSON(path::String)::QCCDevDescription
     catch err
         throw(ArgumentError(err.msg))
     end
+end
+
+"""
+Fills OperationTimes global variable from JSON file.
+Throws ArgumentError if input is not a valid file.
+Throws ArgumentError if there is a negative time value.
+Throws ArgumentError if file contents are not of the format Dict{String, Int}.
+"""
+function readTimeJSON(path ::String)
+    if !isfile(path)
+        throw(ArgumentError("Input is not a file"))
+    end
+    # Parsing JSON
+    times = try 
+        JSON3.read(read(path, String), Dict{Symbol, Int64})
+    catch err
+        throw(ArgumentError(err.msg))
+    end
+    isempty(filter(v -> v < 0, collect(values(times)))) ||
+        throw(ArgumentError("Time values can't be negative"))
+
+    setOperationTimes(times)
 end
 
 end
